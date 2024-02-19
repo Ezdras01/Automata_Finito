079db74b7f1e739a2dc5e4f17c519a58d73d5c71
Ezdras <lehi170993@gmail.com>
2024-02-19 09:55:13 -0600

    Estados y alfabeto
    Se agregaron los estados y el alfabeto  ademas

--- Atumoata_Finito.py
+++ Atumoata_Finito.py
@@ -6,9 +6,12 @@ menu="""
 Bienvendio al automata finito ingresa los datos que se te pedirán a continuacion:
 """
 print(menu)
+
 while True:
     try:
         estados = int(input("Ingresa los estados que tendra tu automata (Q): "))
+        print("El automata tendra", estados, "estados")
+        
     except ValueError:
         print("Por favor ingresa un número")
       
@@ -19,3 +22,22 @@ while True:
         continue
     else:
         break
+
+alfabeto = str(input("Ingresa el alfabeto que tendra tu automata, sepralos por comas (Σ): "))
+print("El automata tendra el siguiente alfabeto:", alfabeto)
+
+while True:
+    try:
+        inicio = int(input("Ingresa el estado inicial de tu automata (q0): "))        
+    except ValueError:
+        print("Por favor ingresa un número")
+        continue
+    if inicio <= 0 or inicio > estados:
+        print("Por favor ingresa un número valido para el estado inicial")
+        continue
+    
+    else:
+        print("El estado inicial de tu automata es: q",inicio)
+        break
+   
+   
